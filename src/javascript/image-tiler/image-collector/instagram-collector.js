938dbf95ff3713270f5bcaddf3cd9dccc9657c74
nogahighland <noga.highland@gmail.com>
2017-08-26 16:21:09 +0900

    fix instagram image selector

--- src/javascript/image-tiler/image-collector/instagram-collector.js
+++ src/javascript/image-tiler/image-collector/instagram-collector.js
@@ -3,7 +3,7 @@ import Collector from './collector'
 class InstagramCollector extends Collector {
   canHandle() { return /^https:\/\/www.instagram.com/.test(location.href); }
 
-  getSelector() { return '._icyx7'; }
+  getSelector() { return '._2di5p'; }
 
   getUrl($dom) {
     return $dom.attr('src').replace(/\/[a-z]\d+x\d+\//, '/');
