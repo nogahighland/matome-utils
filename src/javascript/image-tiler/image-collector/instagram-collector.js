057b669ac9ae9a2c0b52cb9ea794205facfa1844
nogahighland <noga.highland@gmail.com>
2018-07-27 02:27:07 +0900

    update instagram dom structure

--- src/javascript/image-tiler/image-collector/instagram-collector.js
+++ src/javascript/image-tiler/image-collector/instagram-collector.js
@@ -3,10 +3,10 @@ import Collector from './collector'
 class InstagramCollector extends Collector {
   canHandle() { return /^https:\/\/www.instagram.com/.test(location.href); }
 
-  getSelector() { return '._2di5p'; }
+  getSelector() { return '.KL4Bh > img'; }
 
   getUrl($dom) {
-    return $dom.attr('src').replace(/\/[a-z]\d+x\d+\//, '/');
+    return $dom.attr('src');
   }
 }
 
