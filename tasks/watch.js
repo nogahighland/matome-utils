424f5fe4e7a86f49fc6e05d39f3821acf62fe422
nogahighland <noga.highland@gmail.com>
2019-09-12 23:24:45 +0900

    update gulp 4.0.2

--- tasks/watch.js
+++ tasks/watch.js
@@ -2,6 +2,6 @@
 import gulp from 'gulp';
 
 gulp.task('watch', () => {
-  gulp.watch(['./src/javascript/**/*.js'], ['js']);
-  gulp.watch(['./src/manifest.json'], ['json']);
+  gulp.watch(['./src/javascript/**/*.js'], gulp.task('js'));
+  gulp.watch(['./src/manifest.json'], gulp.task('json'));
 });
